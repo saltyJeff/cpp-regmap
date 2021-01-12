e051c8d9f2b9d70a80421e1cd53b85b08353038b
saltyJeff <saltyJeff@users.noreply.github.com>
2021-01-11 21:12:33 -0800

    made it easier to change bitset size

--- include/regmap/bitset.h
+++ include/regmap/bitset.h
@@ -2,21 +2,25 @@
 #include <cstdint>
 #include <type_traits>
 namespace regmap {
-	// a simple bitset implementation.
+	/*
+	 * A simple bitset implementation
+	 */
 
 	// use system integer size to reduce offset-addressing
-	static constexpr std::size_t INT_SIZE = sizeof(int);
+	using REGMAP_BACKING_TYPE = int;
+
+	static constexpr std::size_t INT_SIZE = sizeof(REGMAP_BACKING_TYPE);
 
 	constexpr unsigned int requiredInts(unsigned int nBits) {
 		return (nBits + INT_SIZE - 1) / INT_SIZE;
 	}
 	template<unsigned int N>
-	using bitset = int[requiredInts(N)];
+	using bitset = REGMAP_BACKING_TYPE[requiredInts(N)];
 
-	inline bool bitset_test(const int *bitset, unsigned int bit) {
+	inline bool bitset_test(const REGMAP_BACKING_TYPE *bitset, unsigned int bit) {
 		return bitset[bit / INT_SIZE] & (1 << (bit % INT_SIZE));
 	}
-	inline void bitset_set(int *bitset, unsigned  int bit) {
+	inline void bitset_set(REGMAP_BACKING_TYPE *bitset, unsigned  int bit) {
 		bitset[bit / INT_SIZE] |= 1 << (bit % INT_SIZE);
 	}
 }
--- test/compileSize.cpp
+++ test/compileSize.cpp
@@ -3,17 +3,21 @@
 /**
  * A small function to make sure the compile size is OK
  */
+
+DECLR_REG(WORD_1_REG, 0x11, uint16_t);
 int main() {
 	DummyBus bus;
-	Regmap<endian::big, ONE_REG> testMap(&bus, 0);
+	Regmap<endian::big, ONE_REG, WORD_1_REG> testMap(&bus, 0);
 
 	uint8_t x;
 	testMap.read<ONE_REG>(x);
 
-	uint16_t a, b;
+	uint16_t a;
 	testMap.read<WORD_BYTE_H>(a);
 
 	// print all the variables we used, so the compiler can't optimize them out
-	printf("screw u optimizing compiler: %x, %x, %x\n", x, a, b);
+	printf("screw u optimizing compiler: %x, %x, %x\n", x, a);
+
+	printf("Sizeof memo: %d\n", sizeof(testMap.memoized));
 	return 0;
 }
